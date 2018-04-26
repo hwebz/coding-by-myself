b2191074e14afe815d377d6db94c4210987a9ca3
Ha Do Manh <hadm@goquo.com>
2018-04-26 10:25:00 +0700

    Continue learning

--- pluralsight/react-fundamental/1.quiz-app/app.js
+++ pluralsight/react-fundamental/1.quiz-app/app.js
@@ -3,14 +3,29 @@
 
 	var Quiz = React.createClass({
 		render: function() {
-			return <div>{this.props.data}</div>;
+			return (
+				<div>
+					{this.props.books.map(function(b) {
+						return <Book title={b} />
+					})}
+				</div>
+			);
 		}
 	});
 
 	var Book = React.createClass({
+		propTypes: {
+			title: React.propTypes.string.isRequired
+		},
 		render: function() {
 			return(
-
+				<div>
+					<div className="row">
+						<div className="col-md-4">
+							<img src={this.state.author.imageUrl} className="" />
+						</div>
+					</div>
+				</div>
 			);
 		}
 	});
