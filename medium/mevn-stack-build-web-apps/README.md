d80d7397e4c8c69ae9e7def96e044cfad7f206cb
Ha Do Manh <hadm@goquo.com>
2018-05-24 18:20:29 +0700

    learning MEVN building web apps on Medium (not done yet)

---
+++ medium/mevn-stack-build-web-apps/README.md
@@ -0,0 +1,1 @@
+URL: https://medium.com/@anaida07/mevn-stack-application-part-1-3a27b61dcae0
---
+++ medium/mevn-stack-build-web-apps/posts/client/src/components/Posts.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+    This file will list all the posts.
+
+    <div v-for="(post, postIndex) in posts" :key="postIndex">
+      <p>
+        <span><b>{{post.title}}</b></span><br />
+        <span>{{ post.description }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostsService from '@/services/PostsService'
+
+export default {
+  name: 'Posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data
+    }
+  }
+}
+</script>
