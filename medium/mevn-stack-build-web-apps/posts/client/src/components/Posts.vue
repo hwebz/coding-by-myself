8e4c024f3de5cf0e97ca68821e2bf4960b7a85fb
Ha Do Manh <hadm@goquo.com>
2018-05-25 11:50:06 +0700

    finish learning mevn stack building web apps

--- medium/mevn-stack-build-web-apps/posts/client/src/components/Posts.vue
+++ medium/mevn-stack-build-web-apps/posts/client/src/components/Posts.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="posts">
     <h1>Posts</h1>
-    This file will list all the posts.
-
-    <div v-for="(post, postIndex) in posts" :key="postIndex">
-      <p>
-        <span><b>{{post.title}}</b></span><br />
-        <span>{{ post.description }}</span>
-      </p>
+    <div v-if="posts.length > 0" class="table-wrap">
+      <div>
+        <router-link :to="{name: 'NewPost'}">Add Post</router-link>
+      </div>
+      <table>
+        <tr>
+          <td>Title</td>
+          <td width="550">description</td>
+          <td width="100" align="center">Action</td>
+        </tr>
+        <tr v-for="(post, postIndex) in posts" :key="postIndex">
+          <td>{{post.title}}</td>
+          <td>{{post.description}}</td>
+          <td align="center">
+            <router-link :to="{name: 'EditPost', params: {id: post._id}}">Edit</router-link>
+            <a href="#" @click.prevent="deletePost(post._id)">Delete</a>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div v-else>
+      <p>There are no posts.. Lets add one now</p>
+      <router-link :to="{name: 'NewPost'}" class="add_post_link">Add Post</router-link>
     </div>
   </div>
 </template>
 
 <script>
 import PostsService from '@/services/PostsService'
+import _ from 'lodash'
 
 export default {
   name: 'Posts',
@@ -28,8 +45,57 @@ export default {
   methods: {
     async getPosts () {
       const response = await PostsService.fetchPosts()
-      this.posts = response.data
+      this.posts = response.data.posts
+    },
+    async deletePost (id) {
+      await PostsService.deletePost(id)
+
+      // have to call ajax each time delete
+      // this.getPosts()
+
+      // Much preferable
+      this.posts = _.remove(this.posts, (post) => {
+        return post._id !== id
+      })
     }
   }
 }
 </script>
+<style type="text/css">
+  .table-wrap {
+    width: 60%;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .table-wrap table {
+    margin: 15px auto;
+  }
+  table th, table tr {
+    text-align: left;
+  }
+  table thead {
+    background: #f2f2f2;
+  }
+  table tr td {
+    padding: 10px;
+  }
+  table tr:nth-child(odd) {
+    background: #f2f2f2;
+  }
+  table tr:nth-child(1) {
+    background: #4d7ef7;
+    color: #fff;
+  }
+  a {
+    color: #4d7ef7;
+    text-decoration: none;
+  }
+  a.add_post_link {
+    background: #4d7ef7;
+    color: #fff;
+    padding: 10px 80px;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: bold;
+  }
+</style>
--- medium/mevn-stack-build-web-apps/posts/server/src/app.js
+++ medium/mevn-stack-build-web-apps/posts/server/src/app.js
@@ -3,18 +3,95 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/posts')
+
+var db = mongoose.connection
+
+db.on('error', console.error.bind(console, "connection error"))
+db.once('open', (callback) => {
+    console.log("Connection Succeeded")
+})
+
+var Post = require('../models/post')
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/posts', (req, res) => {
-    res.send(
-        [{
-            title: "Hello world",
-            description: "Hi there! How are you? dasd asd"
-        }]
-    )
+    // res.send(
+    //     [{
+    //         title: "Hello world",
+    //         description: "Hi there! How are you? dasd asd"
+    //     }]
+    // )
+    Post.find({}, 'title description', (error, posts) => {
+        if (error) console.error(error)
+
+        res.send({
+            posts: posts
+        })
+    }).sort({_id: -1})
+})
+
+app.post('/posts', (req, res) => {
+    var db = req.db
+    var title = req.body.title
+    var description = req.body.description
+
+    var new_post = new Post({
+        title: title,
+        description: description
+    })
+    console.log(req.body);
+    new_post.save((error) => {
+        if (error) console.error(error)
+
+        res.send({
+            success: true,
+            message: 'Post saved successfully!'
+        })
+    })
+})
+
+app.get('/post/:id', (req, res) => {
+    var db = req.db
+    Post.findById(req.params.id, 'title description', (error, post) => {
+        if (error) console.error(error)
+
+        res.send(post)
+    })
+})
+
+app.post('/posts/:id', (req, res) => {
+    var db = req.db
+    Post.findById(req.params.id, 'title description', (error, post) => {
+        if (error) console.error(error)
+
+        post.title = req.body.title
+        post.description = req.body.description
+        post.save((error) => {
+            if (error) console.error(error)
+
+            res.send({
+                success: true
+            })
+        })
+    })
+})
+
+app.delete('/posts/:id', (req, res) => {
+    var db = req.db
+    Post.remove({
+        _id: req.params.id
+    }, function(error, post) {
+        if (error) res.send(error)
+        res.send({
+            success: true
+        })
+    })
 })
 
 app.listen(process.env.PORT || 8081);
