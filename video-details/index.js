a2ce183dfa2e59701ce08192f65c25492d796305
Ha Do Manh <hadm@goquo.com>
2019-01-16 16:45:01 +0700

    add some links

--- video-details/index.js
+++ video-details/index.js
@@ -1,7 +1,7 @@
-const testFolder = '../../Videos/data-science-and-machine-learning-with-python-hands-on/';
+const testFolder = '../../Done/Packt.Step.by.Step.Machine.Learning.with.Python/';
 const fs = require('fs');
 const VideoLib = require('node-video-lib');
-var stream = fs.createWriteStream('durations.txt');
+var stream = fs.createWriteStream('durations2.txt');
 var duration = 0;
 
 function durationConverter(duration) {
@@ -15,23 +15,26 @@ function durationConverter(duration) {
 
     return '' + (hours > 0 ? ((hours > 9 ? hours : '0' + hours) + ':') : '') + (minutes > 9 ? minutes : '0' + minutes) + ':' + (seconds > 9 ? seconds : '0' + seconds)
 }
+var i = 0;
 stream.once('open', function() {
     fs.readdir(testFolder, (err, files) => {
         files.sort();
-        console.log(files);
-        files.forEach(file => {
-            fs.open(testFolder + file, 'r', function(err, fd) {
-                try {
-                    let movie = VideoLib.MovieParser.parse(fd);
-                    // Work with movie
-                    stream.write(durationConverter(duration) + " " + file.replace('.mp4', '') + '\n');
-                    duration += movie.relativeDuration()
-                } catch (ex) {
-                    console.error('Error:', ex);
-                } finally {
-                    fs.closeSync(fd);
-                }
+        setTimeout(function() {
+            files.forEach(file => {
+                fs.open(testFolder + file, 'r', function(err, fd) {
+                    try {
+                        let movie = VideoLib.MovieParser.parse(fd);
+                        // Work with movie
+                        stream.write(durationConverter(duration) + " " + file.replace('.mp4', '') + '\n');
+                        duration += (movie.relativeDuration())
+                        i++;
+                    } catch (ex) {
+                        console.error('Error:', ex);
+                    } finally {
+                        fs.closeSync(fd);
+                    }
+                });
             });
-        });
+        }, 1000);
     });
 });
