e975431e00cdcfde77c1e70235adb3d404f2aa22
Ha Do Manh <hadm@goquo.com>
2019-01-29 13:46:57 +0700

    learn django & vue to make CRUD app

---
+++ medium/crud-app-using-vuejs-and-django/crud-app-vuejs-django/README.md
@@ -0,0 +1,33 @@
+# CRUD-APP-vuejs-django
+This repo contains the code used to in the tutorial on the [medium blog](https://medium.com/@shubhambansal_89125/crud-app-using-vue-js-and-django-516edf4e4217)
+
+# Getting Started
+
+```sh
+$ git clone https://github.com/ShubhamBansal1997/crud-app-vuejs-django.git
+$ cd crud-app-vuejs-django/
+$ virtualenv venv
+$ venv/bin/activate
+$ pip install -r requirements.txt
+$ cd myproject
+$ python manage.py migrate
+$ python manage.py runserver 2100
+```
+Application is started at 
+```sh
+127.0.0.1:2100
+```
+
+License
+----
+
+MIT
+
+***
+
+Contributors
+---
+If you would any error or issue you can mention in issues or raise a pull request.
+
+Made with ❤
+
--- medium/crud-app-using-vuejs-and-django/crud-app-vuejs-django/myproject/myproject/urls.py
+++ medium/crud-app-using-vuejs-and-django/crud-app-vuejs-django/myproject/myproject/urls.py
@@ -1,7 +1,7 @@
 """myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .routers import router
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('article', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
+
 ]
